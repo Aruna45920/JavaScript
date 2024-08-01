563caba42637fce4864db41a4a2915659975a141
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-01 12:12:13 +0530

    JavaScript

---
+++ operators.js
@@ -0,0 +1,48 @@
+
+// Arthmetic operators
+
+  var a=10;
+  var b=20;
+  console.log(a+b)
+ var a=10;
+ var b=20;
+ console.log(a-b)
+ console.log(a*b)
+ console.log(a/b)
+ console.log(a%b)
+ console.log(a**b)// power
+ a--;
+ console.log(a)
+ a++;
+ console.log(a)
+
+//---------------------------------------
+
+// Assignment operators
+ var a=10;
+ var b=20;
+ a+=b;
+ console.log(a)
+
+var a=10;
+var b=20;
+a-=b;
+console.log(a)
+
+//-----------------------------------------
+
+// Comparison operators
+
+//String Operators
+
+var str1="Ammu"
+var str2="Junnu"
+
+console.log(str1+str2)
+
+//Logical operators
+var a=10;
+var b=20;
+console.log(!(a==b))
+
+
