01a186bf1e067612bd1f4c3cd0498c621a6114bc
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-01 12:11:23 +0530

    JavaScript

---
+++ Datatypes.js
@@ -0,0 +1,54 @@
+// number datatypes
+ var a = 10;
+ console.log(a)
+ console.log(typeof(a))
+
+var a=10.22;
+console.log(a)
+console.log(typeof(a))
+
+// ----------------------------
+// string datatypes
+var a="H";
+console.log(a)
+console.log(typeof(a))
+
+// -----------------------------
+// boolean datatypes
+var a=true;
+console.log(a)
+console.log(typeof(a))
+
+// ----------------------------------
+// null datatypes
+var a=null
+console.log(a)
+console.log(typeof(a))
+
+// ----------------------------------------
+// undefined datatypes
+var a;
+console.log(a)
+console.log(typeof(a))
+
+// ------------------------------
+// Array datatypes
+ var ar=[1,2,3,4,5]
+ console.log(ar)
+ console.log(typeof(ar))
+
+var arr=[1,11.2,"H",true]
+console.log(arr)
+
+// ----------------------------
+
+// object datatypes
+
+var obj={
+    name:"Ammu",
+    age:10
+}
+console.log(obj)
+console.log(typeof(obj))
+
+
