22605ebb7b1531951b170463b516c6fead320a8d
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-02 16:22:49 +0530

    Dom_Jsfile

---
+++ Dom.js
@@ -0,0 +1,13 @@
+// document.getElementById("p1").innerHTML="This is a paragraph tag"
+// document.getElementById("h1").style.color="red"
+// document.getElementById("img1").src="image1.png"
+
+// addEventListener(Event,function)
+
+//Event-> click,mouseover,mouseout etc..
+
+
+document.getElementById("p1").addEventListener("click",function(){
+document.write("you will get your dream job")
+    
+})
