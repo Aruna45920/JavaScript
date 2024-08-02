3ac44d437e39ad7feb6bc5c60f1682f1f54715c5
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-02 12:11:06 +0530

    Events.js

---
+++ events.js
@@ -0,0 +1,20 @@
+
+// function display() {
+//     alert("This is an onclick event")
+// }
+
+
+// function display() {
+//     alert("This is an onmouseover event")
+// }
+
+
+// function display() {
+//     alert("This is an onmouseout event")
+// }
+
+
+function display(inp) {
+    alert("The modified name is " +" "+ inp)
+
+}
