932e0ce35295fa4306c5971e5af8048aaedc204f
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-08-02 13:03:26 +0530

    Object_Creation

---
+++ object.js
@@ -0,0 +1,33 @@
+// one way of creating object
+
+// var obj = {
+//     firstName : "Aruna",
+//     lastName : "Kumari",
+//     age : 22,
+  
+// }
+
+// console.log(obj)
+
+//-----------------------------------------`
+
+// another way of creating object
+
+// var obj ={}
+
+// obj.firstName = "Aruna"
+// obj.lastName = "Kumari"
+// obj.age = 21
+
+// console.log(obj)
+
+//-------------------------------------------
+
+// another way of creating object
+
+var obj = new Object()
+obj.firstName = "Reddi"
+obj.lastName = "Rani"
+obj.age = 22
+
+console.log(obj)
